3aa2bf98a8ed839219720337c9cec2c2b77779f0
monnand <monnand@gmail.com>
2013-04-27 17:02:52 -0400

    removed obsoleted code

--- src/org/uniqush/client/CommandHandler.java
+++ src/org/uniqush/client/CommandHandler.java
@@ -2,6 +2,4 @@ package org.uniqush.client;
 
 interface CommandHandler {
 	public void OnCommand(Command cmd);
-	public void OnKeyExchangeError(String reason);
-	public void OnAuthenticationError();
 }
