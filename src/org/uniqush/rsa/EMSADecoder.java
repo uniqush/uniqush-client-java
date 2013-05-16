33500a3ab220c9398d41eb9e3e6cdb5cc4e209a5
monnand <monnand@gmail.com>
2013-05-16 03:49:56 -0400

    license

--- src/org/uniqush/rsa/EMSADecoder.java
+++ src/org/uniqush/rsa/EMSADecoder.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2013 Nan Deng
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.uniqush.rsa;
 
 import java.security.MessageDigest;
@@ -16,7 +33,7 @@ class EMSADecoder {
 		}
 	}
 	
-	public boolean decode(byte[] hashed, byte[] EM, int emBits, int saltLen) {
+	public boolean decode(byte[] hashed, byte[] em, int emBits, int saltLen) {
 		if (saltLen < 0) {
 	    	throw new IllegalArgumentException("salt length should not less than zero");
 		}
@@ -30,18 +47,18 @@ class EMSADecoder {
 		}
 		int emLen = (emBits + 7) / 8;
 		
-		if ((EM[EM.length - 1] & 0xFF) != 0xBC) {
+		if ((em[em.length - 1] & 0xFF) != 0xBC) {
 			return false;
 		}
-		if ((EM[0] & (0xFF << (8 - (8 * emLen - emBits)))) != 0) {
+		if ((em[0] & (0xFF << (8 - (8 * emLen - emBits)))) != 0) {
 			return false;
 		}
 		
 		byte[] DB = new byte[emLen - hLen - 1];
 		byte[] H = new byte[hLen];
 		
-		System.arraycopy(EM, 0, DB, 0, emLen - hLen - 1);
-		System.arraycopy(EM, emLen - hLen - 1, H, 0, hLen);
+		System.arraycopy(em, 0, DB, 0, emLen - hLen - 1);
+		System.arraycopy(em, emLen - hLen - 1, H, 0, hLen);
 		
 		byte[] dbMask = mgf.generateMask(H, emLen - hLen - 1);
 		
